e19d279f13e6e6b733c5ea15fc640842391b2d04
Kevin-Insinna <kji8379@rit.edu>
2024-02-23 17:38:34 -0500

    Fixed bug with TitleScreenBehavior Script

--- Assets/Scripts/UI Scripts/TitleScreenBehavior.cs
+++ Assets/Scripts/UI Scripts/TitleScreenBehavior.cs
@@ -11,7 +11,7 @@ public class TitleScreenBehavior : MonoBehaviour
     //Transitions to the Game Scene
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene(1);
     }
 
     //Quits the Game
